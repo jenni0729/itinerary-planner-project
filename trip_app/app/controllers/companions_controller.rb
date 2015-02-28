a568ab1c76498aaac10ccaf65b602d31e9d1c284
George Navas <gannavas@gmail.com>
2015-02-28 15:19:26 -0800

    added view files to companions

--- trip_app/app/controllers/companions_controller.rb
+++ trip_app/app/controllers/companions_controller.rb
@@ -1,5 +1,10 @@
 class CompanionsController < ApplicationController
+  
+  before_action :check_is_activity
+  before_action :find_companion, only: [:show, :edit, :update]
+
   def show
+
   end
 
   def edit
@@ -11,8 +16,29 @@ class CompanionsController < ApplicationController
   def index
   end
 
-  def full_name
-
+  def create
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def check_is_activity
+    if companion_params[:activity_id] == nil
+      is_activity == nil
+    end 
+  end
+
+  def find_companion
+    @companion = Companion.find params[:id]
+  end
+
+  private
+
+  def companion_params
+    params.require(:user).permit(:email, :first_name, :last_name, :activity_id, :companion_id, :itinerary_id)
+  end 
 
 end

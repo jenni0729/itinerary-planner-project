5ff1b32949cd4ebb6e5b6cb3a294ae6d4a6f6c2d
rycwilson <rycwilson@gmail.com>
2015-02-28 12:14:49 -0800

    more progress on activities

--- trip_app/app/controllers/activities_controller.rb
+++ trip_app/app/controllers/activities_controller.rb
@@ -1,8 +1,14 @@
 class ActivitiesController < ApplicationController
 
+  before_action :find_itinerary, only: [:index, :new, :show, :edit, :create, :update, :destroy]
+
   def index
-    @itinerary = Itinerary.find params[:itinerary_id]
+    binding.pry
     @activities = @itinerary.activities
+  end
+
+  def new
+    @activity = Activity.new
   end
 
   def show
@@ -11,8 +17,26 @@ class ActivitiesController < ApplicationController
   def edit
   end
 
-  def new
+  def create
+    @activity = Activity.new activity_params
+    if @activity.save
+      redirect_to itinerary_activities_path(@itinerary)
+    else
+      flash.now[:alert] = "Please correct the following input errors"
+      render :new
+    end
   end
+
+  private
+
+  def find_itinerary
+    @itinerary = Itinerary.find params[:itinerary_id]
+  end
+
+  def activity_params
+    params.require(:activity).permit(:name, :location, :date, :itinerary_id)
+  end
+
 
 
 end

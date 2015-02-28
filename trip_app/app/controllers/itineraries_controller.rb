e6b7d340c33ac918a65b49939f68e08487554b4a
rycwilson <rycwilson@gmail.com>
2015-02-27 23:40:57 -0800

    full crud on itineraries, some styling modifications

--- trip_app/app/controllers/itineraries_controller.rb
+++ trip_app/app/controllers/itineraries_controller.rb
@@ -1,13 +1,57 @@
 class ItinerariesController < ApplicationController
-  def show
-  end
 
-  def edit
+  # before_action :find_itinerary, only: [:show, :edit, :destroy, :add_comment, :remove_comment]
+  before_action :find_itinerary, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @itineraries = Itinerary.all
   end
 
   def new
+    @itinerary = Itinerary.new
   end
 
-  def index
+  def show
+    @itinerary_owner = User.find @itinerary.user_id
   end
+
+  def edit
+    @itinerary_owner = User.find @itinerary.user_id
+  end
+
+  def create
+    @itinerary = Itinerary.new itinerary_params
+    if @itinerary.save
+      redirect_to itineraries_path
+    else
+      flash.now[:alert] = "Please correct the following input errors"
+      render :new
+    end
+  end
+
+  def update
+    @itinerary.update_attributes itinerary_params
+    if @itinerary.save
+      redirect_to itinerary_path(@itinerary)
+    else
+      flash.now[:alert] = "Please correct the following input errors"
+      render :edit
+    end
+  end
+
+  def destroy
+    @itinerary.destroy
+    redirect_to itineraries_path
+  end
+
+  private
+
+  def find_itinerary
+    @itinerary = Itinerary.find params[:id]
+  end
+
+  def itinerary_params
+    params.require(:itinerary).permit(:name, :origin, :destination, :departure_date, :return_date, :user_id)
+  end
+
 end

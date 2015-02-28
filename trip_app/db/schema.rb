9c176215be2138b219e1bcf4ec3eddf8c3fac364
George Navas <gannavas@gmail.com>
2015-02-27 19:21:05 -0800

    pushing modification to companions_activities table - changing column name from desination_id to companion_id

--- trip_app/db/schema.rb
+++ trip_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227214910) do
+ActiveRecord::Schema.define(version: 20150228031706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,10 +35,10 @@ ActiveRecord::Schema.define(version: 20150227214910) do
   end
 
   create_table "companions_activities", force: :cascade do |t|
-    t.integer  "destination_id"
+    t.integer  "activity_id"
     t.integer  "companion_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "companions_itineraries", force: :cascade do |t|

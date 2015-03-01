940b0d1456f12b053aaea763bcfceeec439cb93b
George Navas <gannavas@gmail.com>
2015-03-01 13:46:00 -0800

    updated schema

--- trip_app/db/schema.rb
+++ trip_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301191129) do
+ActiveRecord::Schema.define(version: 20150301212927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20150301191129) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.integer  "itinerary_id"
+    t.string   "email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "itineraries", force: :cascade do |t|

6bda7f86758c9b920655264c06e6d08c54aa0946
rycwilson <rycwilson@gmail.com>
2015-03-01 10:46:40 -0800

    latest and greatest

--- trip_app/app/controllers/reviews_controller.rb
+++ trip_app/app/controllers/reviews_controller.rb
@@ -8,22 +8,6 @@ class ReviewsController < ApplicationController
     redirect_to :back
   end
 
-  # def create_activity_review
-  #   unless find_reviewable_activity.reviews.build(review_params).save
-  #     flash[:notice] = "Review validation failed"
-  #   end
-  #   # :back is native?
-  #   redirect_to :back
-  # end
-
-  # def create_itinerary_review
-  #   unless find_reviewable_itinerary.reviews.build(review_params).save
-  #     flash[:notice] = "Review validation failed"
-  #   end
-  #   # :back is native?
-  #   redirect_to :back
-  # end
-
   private
 
   def review_params
@@ -39,25 +23,5 @@ class ReviewsController < ApplicationController
     end
     nil
   end
-
-  # def find_reviewable_itinerary
-  #   params.each do |name, value|
-  #     if name =~ /(.+)_id$/
-  #       binding.pry
-  #       return $1.classify.constantize.find(value)
-  #     end
-  #   end
-  #   nil
-  # end
-
-  # def find_reviewable_activity
-  #   params.each do |name, value|
-  #     if name =~ /(.+)_id$/
-  #       binding.pry
-  #       return $2.classify.constantize.find(value)
-  #     end
-  #   end
-  #   nil
-  # end
 
 end

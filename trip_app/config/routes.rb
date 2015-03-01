25de65b15d5d8c7a8a996246e704715f7257c505
George Navas <gannavas@gmail.com>
2015-03-01 15:45:11 -0800

    update to mandril email api project

--- trip_app/config/routes.rb
+++ trip_app/config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
     resources :companions
   end
 
+  def invite_params
+    params.require(:invite).permit(:itinerary_id, :email)
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

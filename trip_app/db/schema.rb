af8612de0500b3cb524a897095f6fa98814b130b
George Navas <gannavas@gmail.com>
2015-03-01 11:14:15 -0800

    created start_date and end_Date columns for activities

--- trip_app/db/schema.rb
+++ trip_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301003303) do
+ActiveRecord::Schema.define(version: 20150301191129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20150301003303) do
     t.date     "date"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.date     "start_date"
+    t.date     "end_date"
   end
 
   create_table "activities_users", force: :cascade do |t|

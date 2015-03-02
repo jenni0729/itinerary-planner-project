3298f1cc0aa9716f51c833b96866342a0181fca8
rycwilson <rycwilson@gmail.com>
2015-03-01 21:25:08 -0800

    pre-pull commit

--- trip_app/app/controllers/reviews_controller.rb
+++ trip_app/app/controllers/reviews_controller.rb
@@ -8,29 +8,6 @@ class ReviewsController < ApplicationController
     redirect_to :back
   end
 
-  def create_itinerary_review
-    unless find_reviewable_itinerary.reviews.build(review_params).save
-      flash[:notice] = "Review validation failed"
-    end
-    # :back is native?
-    redirect_to :back
-  end
-  # def create_activity_review
-  #   unless find_reviewable_activity.reviews.build(review_params).save
-  #     flash[:notice] = "Review validation failed"
-  #   end
-  #   # :back is native?
-  #   redirect_to :back
-  # end
-
-  # def create_itinerary_review
-  #   unless find_reviewable_itinerary.reviews.build(review_params).save
-  #     flash[:notice] = "Review validation failed"
-  #   end
-  #   # :back is native?
-  #   redirect_to :back
-  # end
-
   private
 
   def review_params
